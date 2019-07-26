a8128deb95a90d7bfa878ea974d5af54ba8b8bba
FoujiaAfifa <f.afifa01@gmail.com>
2019-07-26 21:15:53 +0600

    Linq & Class Practice

---
+++ LINQ Practice/LinqPracticeWebApp/LinqPracticeWebApp/LinqPractice.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LinqPractice.aspx.cs" Inherits="LinqPracticeWebApp.LinqPractice" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style=" font-family:Verdana">
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView> <br />
+            <asp:GridView ID="GridView2" runat="server"></asp:GridView><br />
+            <asp:GridView ID="GridView3" runat="server"></asp:GridView><br />
+            <asp:GridView ID="GridView4" runat="server"></asp:GridView><br />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ MyMVCApp/MyMVCApp/Controllers/StudentController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyMVCApp.BLL.BLL;
+using MyMVCApp.Models.Models;
+
+namespace MyMVCApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentManager _studentManager =new StudentManager();
+        private Student _student = new Student();
+        
+        // GET: Student
+        public string Add(Student student)
+        {
+            //_student.ID = 101;
+            _student.Name = student.Name;
+            _student.Address = student.Address;
+            _student.Age = student.Age;
+            _studentManager.Add(_student);
+            return "Added";
+        }
+        public ActionResult Delete(Student student)
+        {
+            _student.ID = student.ID;
+            _studentManager.Delete(_student);
+            return View();
+        }
+        public ActionResult Update(int id,string name,string address,int age)
+        {
+            _student.ID = id;
+            //Method 1
+
+            _student.Name = name;
+            _student.Address = address;
+            _student.Age = age;
+            _studentManager.Update(_student);
+
+            //Student aStudent = _studentManager.GetByID(_student);
+            //if (aStudent != null)
+            //{
+            //    aStudent.Name = name;
+            //    _studentManager.Update(aStudent);
+
+            //}
+
+            return View();
+        }
+
+
+    }
+}
