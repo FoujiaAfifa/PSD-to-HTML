410292598431121f60a9560aa2860bcd5cf20502
FoujiaAfifa <foujia.afifa01@gmail.com>
2019-08-27 06:30:54 +0600

    new projects

---
+++ CrystalReportPractice/CrystalReportPractice/Controllers/StockReportController.cs
@@ -0,0 +1,30 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalReportPractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrystalReportPractice.Controllers
+{
+    public class StockReportController : Controller
+    {
+        StockManagementDBEntities1 _db = new StockManagementDBEntities1();
+
+        // GET: StockReport
+        public ActionResult StockList()
+        {
+            var stockList = _db.Stocks.ToList();
+
+            return View(stockList);
+        }
+
+        public ActionResult exportReport()
+        {
+            ReportDocument rd = new ReportDocument();
+
+
+        }
+    }
+}
